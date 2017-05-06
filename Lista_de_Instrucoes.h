d5befcd1b05e3f7d861805594c737300ed1490cc
Renan Costa Tavares <r.costa.tavares@gmail.com>
2017-05-06 11:25:30 -0300

    Add files via upload

--- Lista_de_Instrucoes.h
+++ Lista_de_Instrucoes.h
@@ -1,3 +1,6 @@
+#IFNDEF _Lista_de_Instrucoes
+#define _Lista_de_Instrucoes
+
 #include <string>
 #include <iostream>
 
@@ -28,3 +31,60 @@ MULT.nome = "MULT";
 MULT.opcode = "03";
 MULT.num_operandos = 1;
 MULT.prox = &DIV;
+
+DIV.nome = "DIV";
+DIV.opcode = "04";
+DIV.num_operandos = 1;
+DIV.prox = &JMP;
+
+JMP.nome = "JMP";
+JMP.opcode = "05";
+JMP.num_operandos = 1;
+JMP.prox = &JMPN;
+
+JMPN.nome = "JMPN";
+JMPN.opcode = "06";
+JMPN.num_operandos = 1;
+JMPN.prox = &JMPP;
+
+JMPP.nome = "JMPP";
+JMPP.opcode = "07";
+JMPP.num_operandos = 1;
+JMPP.prox = &JMPZ;
+
+JMPZ.nome = "JMPZ";
+JMPZ.opcode = "08";
+JMPZ.num_operandos = 1;
+JMPZ.prox = &COPY;
+
+COPY.nome = "COPY";
+COPY.opcode = "09";
+COPY.num_operandos = 2;
+COPY.prox = &LOAD;
+
+LOAD.nome = "LOAD";
+LOAD.opcode = "10";
+LOAD.num_operandos = 1;
+LOAD.prox = &STORE;
+
+STORE.nome = "STORE";
+STORE.opcode = "11";
+STORE.num_operandos = 1;
+STORE.prox = &INPUT;
+
+INPUT.nome = "INPUT";
+INPUT.opcode = "12";
+INPUT.num_operandos = 1;
+INPUT.prox = &OUTPUT;
+
+OUTPUT.nome = "OUTPUT";
+OUTPUT.opcode = "13";
+OUTPUT.num_operandos = 1;
+OUTPUT.prox = &STOP;
+
+STOP.nome = "STOP";
+STOP.opcode = "14";
+STOP.num_operandos = 0;
+STOP.prox = NULL;
+
+#endif
